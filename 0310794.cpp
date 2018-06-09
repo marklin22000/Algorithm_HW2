0317c301fd36308a5d9743bacaca24a9d04621f1
Mark <marklin22000@gmail.com>
2018-06-09 12:01:17 +0800

    first

---
+++ 0310794.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <ctime>
+#include <unistd.h>
+#include <sys/resource.h>
+#include <math.h>
+#include <stdio.h>
+#include <cstdlib>
+using namespace std;
+
+long S_node = -1;
+long node_num;
+long edge_num;
+
+int main(int argc, char* argv[]){
+
+	/* meansure time */
+	const clock_t start_time = clock();
+	/* Input check */
+	if (argc < 4){
+		cout << "Usage: " << argv[0] << " <input_file> <source node> <output_file>" << endl;
+		return -1;
+	}
+	
+	/* File check */
+	ifstream ifs(argv[1]);
+	if (!ifs.is_open()){
+		cout << "Input file: " << argv[1] << " cannot be opened." << endl;
+		return -1;
+	}
+	
+	S_node = atoi(argv[2]);
+
+	/* testing */
+	printf("%d\n",S_node);
+
+	/* File check */
+	ofstream ofs(argv[3]);
+	if (!ofs.is_open()){
+		cout << "Output file: " << argv[3] << " cannot be opened." << endl;
+		return -1;
+	}
+	
+	/* input node number & edge number*/
+	ifs >> node_num;
+	ifs >> edge_num;
+	long *node_ID1;
+	node_ID1= new long [edge_num];
+	long *node_ID2;
+	node_ID2= new long [edge_num];
+	long *weight;
+	weight= new long [edge_num];
+
+	for(int i=0; i<edge_num)
+	{
+		weight[i] = -9999;
+	}
+
+	i=0;
+	/* input the element in the file*/
+	while (!ifs.eof()){
+		long tmp_num;
+
+		ifs >> tmp_num;
+		node_ID1[i] = tmp_num;
+
+		ifs >> tmp_num;
+		node_ID2[i] = tmp_num;
+
+		ifs >> tmp_num;
+		weight[i] = tmp_num;
+
+		i++;
+	}
+	ifs.close();
+	
+	
+	/* Function */
+
+	sort(num,a);
+
+	double run_time = (double)(clock() - start_time) / CLOCKS_PER_SEC;
+
+	struct rusage r_usage;
+	getrusage(RUSAGE_SELF,&r_usage);
+	printf("Memory usage: %ld k-bytes\n",r_usage.ru_maxrss);
+	
+	cout << "=====  Answer Report  =====" << endl;
+	cout << "CPU Run time: " << run_time << endl;
+	cout << "Mem Usage:    " << r_usage.ru_maxrss << endl;
+	
+	ofs << "=====  Answer Report  =====" << endl;
+	ofs << "CPU Run time: " << run_time << endl;
+	ofs << "Mem Usage:    " << r_usage.ru_maxrss << endl;
+	ofs << "ans: "<<endl;
+	for (int i = 0; i < num; i++)		
+		ofs << a[i] <<" ";
+		
+	ofs.close();
+	
+	return 0;
+}
