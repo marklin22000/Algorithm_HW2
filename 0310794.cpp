ed2e257c90c0bd1915015760276541d04a5b819d
Mark <marklin22000@gmail.com>
2018-06-16 21:29:36 +0800

    test2

--- 0310794.cpp
+++ 0310794.cpp
@@ -230,7 +230,7 @@ int main(int argc, char* argv[]){
 			continue;
 		}
 		*/
-		printf(" %6d %6l\n",*it,ans[i]);
+		printf(" %6d %6lf\n",*it,ans[i]);
 	}	
 	
 	ofs << "=====  Answer Report  =====" << endl;
