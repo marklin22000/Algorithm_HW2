b833e142ef6f816baf263a0a9d67c07147dd69e2
Mark <marklin22000@gmail.com>
2018-06-16 22:00:08 +0800

    test2

--- 0310794.cpp
+++ 0310794.cpp
@@ -25,14 +25,6 @@ typedef struct {
 	Edge *edge;	//array of edges
 } graph;
 
-void displayArray(double arr[], int size) {
-	int i;
-	for (i = 0; i < size; i ++) {
-		printf("%lf ", arr[i]);
-	}
-	printf("\n");
-}
-
 void  SSSP(double *ans[], graph *g, int S_node)
 {
 	int i, j, u, v;
@@ -40,58 +32,52 @@ void  SSSP(double *ans[], graph *g, int S_node)
 	int tV = g->V;
 	int tE = g->E;
 	
-	//distance array
-	//int d[tV];
+	/* distance array */
 	double *d;
 	d= new double [tV];
-	//predecessor array
-	//int p[tV];
-	//double *p;
-	//p= new double [tV];
-
-	//step 1: initilaize graph: d[i] and p[i]
-	for (i = 0; i < tV; i++) {
+	
+	/* initilaize d[i] */
+	for (i = 0; i < tV; i++)
 		d[i] = INT_MAX;
-		//p[i] = 0;
-	}
-	//special case: source
+
+	/* source node d[i] */
 	d[S_node] = 0;
 	
-	//step 2: relax edges |V| - 1 times
-	for (i = 1; i <= tV-1; i++) {
-		for (j = 0; j < tE; j++) {
+	/* relax edges |V| - 1 times */
+	for (i = 1; i <= tV-1; i++) 
+	{
+		for (j = 0; j < tE; j++) 
+		{
 			// retrieve edge data
 			u = g->edge[j].u;
 			v = g->edge[j].v;
 			w = g->edge[j].w;
-			// relax d[v] and p[v] if d[u]+w is smaller than current d[v]
-			if (d[u]!= INT_MAX && d[v]>d[u]+w) {
+			// relax d[v] if d[u]+w is smaller than current d[v]
+			if (d[u]!= INT_MAX && d[v]>d[u]+w)
 				d[v] = d[u] + w;
-				//p[v] = u;
-			}
 		}
 	}
 	
-	//step 3: detect negative cycle
-	for (i = 0; i < tE; i++) {
+	/* detect negative cycle */
+	for (i = 0; i < tE; i++) 
+	{
 		u = g->edge[i].u;
 		v = g->edge[i].v;
 		w = g->edge[i].w;
-		if (d[u]!= INT_MAX && d[v]>d[u]+w) {
+		if (d[u]!= INT_MAX && d[v]>d[u]+w) 
+		{
 			printf("[Error] A negative-weight cycle detected!\n");
 			return ;
 		}
 	}
+
+	/* if can't find the path, replaced distance by -9999 */
 	for(i=0;i<tV;i++)
 	{
 		if( d[i] == INT_MAX )
 			d[i] = -9999;
 	}
 
-	//No negative weight cycle found!
-	//print the distance and predecessor array
-	//printf("final distances: ");
-	//displayArray(d, tV);
 	*ans = d;
 }
 
@@ -131,116 +117,80 @@ int main(int argc, char* argv[]){
 	ifs >> dummy;
 	ifs >> g->E;	//edge number
 
+	//allocate space for edge
 	g->edge = (Edge*)malloc(g->E * sizeof(Edge));
 
+	/* using set can automatically sort and detect duplicated node when input data */
 	set<int> re_list;
 	set<int>::iterator it;
 
-	//for(it=re_list.begin();it!=re_list.end();it++)
-	//	cout << *it << endl;
-	//cout << "aaaaaa" << endl;
 	int *node_ID1;
 	node_ID1= new int [g->E];
 	int *node_ID2;
 	node_ID2= new int [g->E];
+
 	/* input the element in the file*/
 	int i=0;
 	while (!ifs.eof()){
 
 		ifs >> node_ID1[i];
 		re_list.insert(node_ID1[i]);
-		//cout << re_list.size() << endl;
+
 		ifs >> node_ID2[i];
 		re_list.insert(node_ID2[i]);
-		//cout << re_list.size() << endl;
+
 		ifs >> g->edge[i].w;
 		
 		i++;
 	}
 	ifs.close();
 
-	//for(it=re_list.begin();it!=re_list.end();it++)
-	//	cout << *it << endl;
-
-	//cout << re_list.size() << endl;
+	/* there had an weird bug, I always got a "0" at the very beginning of the whole set, */
+	/* so I decide to remove it here. */
 	re_list.erase(re_list.begin());
+
+	/* get the index of rearranged nodes */
 	for(i=0; i<g->E; i++)
 	{
 		it = re_list.find(node_ID1[i]);
-		//if(it==re_list.begin())
-			g->edge[i].u = distance(re_list.begin(),it);
-		//else
-		//	g->edge[i].u = distance(re_list.begin(),it)-1;
+		g->edge[i].u = distance(re_list.begin(),it);
 
 		it = re_list.find(node_ID2[i]);
-		//if(it==re_list.begin())
-			g->edge[i].v = distance(re_list.begin(),it);
-		//else
-		//	g->edge[i].v = distance(re_list.begin(),it)-1;
+		g->edge[i].v = distance(re_list.begin(),it);
 	}
-
-	//for(it=re_list.begin();it!=re_list.end();it++)
-	//	cout << *it << endl;
-	//for(int j=0;j< g->E ; j++)
-	//{
-	//	printf("U: %d V: %d W: %lf\n",g->edge[j].u,g->edge[j].v,g->edge[j].w);
-	//}
 
 	/* Function */
 	double *ans;
 	ans = new double [g->V];
 	
+	/* bad input */
 	it = re_list.find(S_node);
 	if(it == re_list.end())
-		printf("Not a valid node!\n");
+	{
+		printf("%d node is not found in %s!\n",S_node,argv[1]);
+		return 0;
+	}
 	else
 		SSSP(&ans,g,distance(re_list.begin(),it));
-	//displayArray(ans, g->V);
-
-	/*
-	for(it=re_list.begin();it!=re_list.end();it++)
-		cout << *it << endl;
-	for(int j=0;j< g->E ; j++)
-	{
-		printf("U: %d V: %d W: %lf\n",g->edge[j].u,g->edge[j].v,g->edge[j].w);
-	}
-	*/
 
 	double run_time = (double)(clock() - start_time) / CLOCKS_PER_SEC;
 
 	struct rusage r_usage;
 	getrusage(RUSAGE_SELF,&r_usage);
-	//printf("Memory usage: %ld k-bytes\n",r_usage.ru_maxrss);
 	
 	cout << "=====  Answer Report  =====" << endl;
 	cout << "Nodes: " << g->V << endl;
 	cout << "CPU Run time: " << run_time << endl;
 	cout << "Mem Usage:    " << r_usage.ru_maxrss << "kB" << endl;
 	for(it=re_list.begin(),i=0;it!=re_list.end();it++,i++)
-	{
-		/*
-		if(it==re_list.begin())
-		{
-			i--;
-			continue;
-		}
-		*/
 		printf("%8d    %-6.1lf\n",*it,ans[i]);
-	}	
 	
 	ofs << "=====  Answer Report  =====" << endl;
 	ofs << "Nodes: " << g->V << endl;
 	ofs << "CPU Run time: " << run_time << endl;
 	ofs << "Mem Usage:    " << r_usage.ru_maxrss << "kB" << endl;
-	ofs << "ans: "<<endl;
-	/*for (int i = 0; i < num; i++)		
-		ofs << a[i] <<" ";
-	*/	
 	for(it=re_list.begin(),i=0;it!=re_list.end();it++,i++)
-	{
 		ofs << *it << "  " << ans[i] << endl;
-		//printf("%8d    %-6.1lf\n",*it,ans[i]);
-	}
 	ofs.close();
 	
 	return 0;
